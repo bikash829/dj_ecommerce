47344a49eb0c82d55adf5553796816014a88040d
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-02-04 11:10:31 +0600

    index file loaded

--- admin/views.py
+++ admin/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'admin/layouts/base_site.html')
+    return render(request, 'admin/pages/index.html')
