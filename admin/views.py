8c853720ed0b3adefbbceb9ed8d8e8aaf78f6250
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-02-04 15:00:51 +0600

    extendable menu create under ui elements > general

--- admin/views.py
+++ admin/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render(request, 'admin/pages/index.html')
+    context = {
+        'home_page_status'   : 'active',
+        'title'         : 'Admin Dashboard',
+        'page'          : 'Dashboard',
+        # 'page_url'      : reverse('app_name:view_name'),
+    }
+    return render(request, 'admin/pages/index.html', context)
+
+
+# page widgets
+def widgets(request):
+    context = {
+        'widgets_page_status'   : 'active',
+        'title'         : 'Widgets',
+        'page'          : 'Widgets',
+        
+    }
+    return render(request, 'admin/pages/widgets.html', context)
+
+# page calendar 
+def calendar(request):
+    context = {
+        'calendar_page_status'      : 'active',
+        'title'                     : 'Calendar',
+        'page'                      : 'Calendar',
+        
+    }
+    return render(request, 'admin/pages/calendar.html', context)
+
+# =============UI Elements================
+# page general 
+def general(request):
+    context = {
+        'general_page_status'       : 'active',
+        'title'                     : 'General UI',
+        'page'                      : 'Inline Charts',
+        'ul_menu'                   : 'menu-open',
+        
+    }
+    return render(request, 'admin/pages/ui/general.html', context)
